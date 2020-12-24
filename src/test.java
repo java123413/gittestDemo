1f557f63e34edf3d35239dbff39329037f084ce9
java123413 <57927359+java123413@users.noreply.github.com>
2020-12-24 16:00:57 +0800

    Update test.java

--- src/test.java
+++ src/test.java
@@ -2,5 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("2222");
+        System.out.println("3333");
+
     }
 }
